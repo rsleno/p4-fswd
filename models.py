44c77701fb0e451097069cdc06df5fb378e099f1
rsanz <roberto.sanz@ticketbis.com>
2015-07-28 14:08:07 +0200

    Adding SessionForms

--- models.py
+++ models.py
@@ -68,7 +68,12 @@ class SessionForm(messages.Message):
     typeOfSession   = messages.StringField(5)
     date            = messages.StringField(6)
     startTime       = messages.StringField(7)
-    conferenceId    = messages.StringField(8)
+    websafeKey      = messages.StringField(8)
+    conferenceId    = messages.StringField(9)
+
+class SessionForms(messages.Message):
+    """SessionForms -- multiple Sessions outbound form message"""
+    items = messages.MessageField(SessionForm, 1, repeated=True)
 
 class Conference(ndb.Model):
     """Conference -- Conference object"""
